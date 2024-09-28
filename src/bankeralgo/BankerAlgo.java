2727284a9df4444d66e742c07d2563d7b3770674
JQuiej <quiej008@gmail.com>
2024-09-27 18:53:16 -0600

    Tarea Subida

---
+++ src/bankeralgo/BankerAlgo.java
@@ -0,0 +1,15 @@
+
+package bankeralgo;
+
+/**
+ *
+ * @author JQuiej
+ */
+public class BankerAlgo {
+
+    public static void main(String[] args) {
+        Home h = new Home();
+        h.setVisible(true);
+    }
+    
+}
